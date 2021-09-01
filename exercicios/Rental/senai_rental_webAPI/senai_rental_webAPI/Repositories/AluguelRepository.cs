19b7db056d067373954039d061d76bb0bc2281f4
Lucas Araujo <lucasaraujo4188@gmail.com>
2021-09-01 12:39:36 -0300

    Adiciona a Repository para a tabela veiculo

--- exercicios/Rental/senai_rental_webAPI/senai_rental_webAPI/Repositories/AluguelRepository.cs
+++ exercicios/Rental/senai_rental_webAPI/senai_rental_webAPI/Repositories/AluguelRepository.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using senai_rental_webAPI.Domains;
+using senai_rental_webAPI.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace senai_rental_webAPI.Repositories
 {
-    public class AluguelRepository
+    public class AluguelRepository : IAluguelRepository
     {
+        private string stringConexao = @"Data source=DESKTOP-PJVB3DK\SQLEXPRESS; initial catalog=rental; integrated security=true";
+        public void Atualizar(AluguelDomain novoAluguel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AluguelDomain BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int idAluguel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir(AluguelDomain aluguel)
+        {
+            
+        }
+
+        public List<AluguelDomain> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- exercicios/Rental/senai_rental_webAPI/senai_rental_webAPI/Repositories/ClienteRepository.cs
+++ exercicios/Rental/senai_rental_webAPI/senai_rental_webAPI/Repositories/ClienteRepository.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using senai_rental_webAPI.Domains;
+using senai_rental_webAPI.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace senai_rental_webAPI.Repositories
 {
-    public class ClienteRepository
+    public class ClienteRepository : IClienteRepository
     {
+        public void Atualizar(ClienteDomain novoCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClienteDomain BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int idCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir(ClienteDomain cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ClienteDomain> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
