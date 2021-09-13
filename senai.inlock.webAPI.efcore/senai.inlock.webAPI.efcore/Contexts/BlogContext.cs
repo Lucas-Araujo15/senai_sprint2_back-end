24102e4d81e4b2bae7d6ace3d573c7ff79cf74dc
Lucas Araujo <lucasaraujo4188@gmail.com>
2021-09-13 20:14:34 -0300

    Primeiros contatos com Entity Framework

---
+++ senai.inlock.webAPI.efcore/senai.inlock.webAPI.efcore/Contexts/BlogContext.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using senai.inlock.webAPI.efcore.Domains;
+
+#nullable disable
+
+namespace senai.inlock.webAPI.efcore.Contexts
+{
+    public partial class BlogContext : DbContext
+    {
+        public BlogContext()
+        {
+        }
+
+        public BlogContext(DbContextOptions<BlogContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Estudio> Estudios { get; set; }
+        public virtual DbSet<Jogo> Jogos { get; set; }
+        public virtual DbSet<TiposUsuario> TiposUsuarios { get; set; }
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data source=DESKTOP-SV3M4A7\\SQLEXPRESS; initial catalog=InLock_Games; user id=sa; pwd=Senai@132 ");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<Estudio>(entity =>
+            {
+                entity.HasKey(e => e.IdEstudio)
+                    .HasName("PK__Estudios__0C3B435529C46433");
+
+                entity.HasIndex(e => e.NomeEstudio, "UQ__Estudios__112A5690D8D602E6")
+                    .IsUnique();
+
+                entity.Property(e => e.NomeEstudio)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Jogo>(entity =>
+            {
+                entity.HasKey(e => e.IdJogo)
+                    .HasName("PK__Jogos__69E085133FE014E6");
+
+                entity.Property(e => e.DataLancamento).HasColumnType("date");
+
+                entity.Property(e => e.DescricaoJogo)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomeJogo)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ValorJogo).HasColumnType("money");
+
+                entity.HasOne(d => d.IdEstudioNavigation)
+                    .WithMany(p => p.Jogos)
+                    .HasForeignKey(d => d.IdEstudio)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Jogos__IdEstudio__398D8EEE");
+            });
+
+            modelBuilder.Entity<TiposUsuario>(entity =>
+            {
+                entity.HasKey(e => e.IdTipoUsuario)
+                    .HasName("PK__TiposUsu__CA04062B16571717");
+
+                entity.Property(e => e.NomeTipoUsuario)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PK__Usuarios__5B65BF976C50DA85");
+
+                entity.HasIndex(e => e.Email, "UQ__Usuarios__A9D105345BC75DE9")
+                    .IsUnique();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomeUsuario)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdTipoUsuarioNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdTipoUsuario)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Usuarios__IdTipo__3F466844");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ senai.inlock.webAPI.efcore/senai.inlock.webAPI.efcore/Domains/Jogo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace senai.inlock.webAPI.efcore.Domains
+{
+    public partial class Jogo
+    {
+        public int IdJogo { get; set; }
+        public string NomeJogo { get; set; }
+        public string DescricaoJogo { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public decimal ValorJogo { get; set; }
+        public int IdEstudio { get; set; }
+
+        public virtual Estudio IdEstudioNavigation { get; set; }
+    }
+}
